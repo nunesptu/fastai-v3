77f76a312a32276de70b4126b48a1b7f4187717e
Welton Rodrigo Torres Nascimento <rodrigo@familianascimento.org>
2019-04-04 14:00:07 -0300

    Remove unused URLs

--- app/server.py
+++ app/server.py
@@ -8,8 +8,6 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-# export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
 export_file_url = 'https://drive.google.com/uc?export=download&id=13Nxml5y0VVrn7J8GjTuxZDO1WwR2YslX'
 export_file_name = 'export.pkl'
 
